05fd02ed26665f9ec0e081742a7cb3528fc7feae
nghiacosy8219 <nghiacosy821@gmail.com>
2022-08-30 09:26:29 +0700

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '1d254da6651b8c84f8dd238b536b057f'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '1b472ae4-704a-4203-b82e-b53ba921ca5b'
+};
